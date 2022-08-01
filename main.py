da11984403b7ccf8ecadc01834a19fd24d13e386
AnastasiaKlimuk <Anastasia_grin@hotmail.com>
2022-08-01 09:37:50 +0300

    test

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 import time 
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"
-
 try:
     browser = webdriver.Chrome()
     browser.get(link)
